271a4defec3f5ad0b5e936e2168605f09193dea0
Tariq Almazyad <tariq.almazyad@gmail.com>
2020-11-30 19:03:54 +0300

    Initial Commit

---
+++ FlickriFy/FlickriFyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickriFyApp.swift
+//  FlickriFy
+//
+//  Created by Tariq Almazyad on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct FlickriFyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
